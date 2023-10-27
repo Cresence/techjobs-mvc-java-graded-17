70ae3b9433d9ad723da8fb5242c72c80667d7abc
Cresence <devharris93@gmail.com>
2023-10-27 16:00:55 -0400

    TODO #1: Job results display in table -- complete.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +27,15 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+//    @GetMapping(value = "jobs")
+//    public String listBySearch(Model model, @RequestParam String column, @RequestParam(required = false) String value){
+//        ArrayList<Job> jobs;
+//        jobs = JobData.findByColumnAndValue(column, value);
+//        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+//
+//        model.addAttribute("jobs", jobs);
+//
+//        return "search-jobs";
+//    }
 }
 
