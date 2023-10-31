3f909e436e0e53d2c8d9533c38bffa2af3b2533b
Cresence <devharris93@gmail.com>
2023-10-30 22:06:54 -0400

    Todo #3 & #4 completed* Search handling and rendering working as intended. Some slight text and css format tinkering to be done

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -5,6 +5,7 @@ import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,15 +28,20 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-//    @GetMapping(value = "jobs")
-//    public String listBySearch(Model model, @RequestParam String column, @RequestParam(required = false) String value){
-//        ArrayList<Job> jobs;
-//        jobs = JobData.findByColumnAndValue(column, value);
-//        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
-//
-//        model.addAttribute("jobs", jobs);
-//
-//        return "search-jobs";
-//    }
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
 
